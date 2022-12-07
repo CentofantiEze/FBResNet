2236b26db1bdcd0586c414209021d4b5dea07645
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-12-07 11:16:16 +0100

    Train the model on kjohnson.

--- jobs/test.sh
+++ jobs/test.sh
@@ -5,26 +5,27 @@ python ../scripts/FBRN_click.py \
     --test_opt True \
     --plot_opt True \
     --dataset_folder ../Datasets/ \
-    --output_folder C:/Users/ezece/OneDrive/Documents/Inria-OPIS/repos/FBResNet/test_dir/ \
+    --output_folder /workspace/repos/FBResNet/test_dir/ \
     --a 1 \
     --p 1 \
     --n 2000 \
     --m 50 \
-    --model_id test_model_000_ \
+    --model_id test_model_001_ \
     --nb_blocks 20 \
     --im_set Set1 \
     --noise 0.05 \
     --constraint cube \
-    --train_size 70 \
-    --val_size 30 \
+    --train_size 400 \
+    --val_size 200 \
     --batch_size 1 \
     --lr 1e-3 \
-    --nb_epochs 15 \
+    --nb_epochs 30 \
     --freq_val 1 \
     --loss_elt False \
     --save_signals False \
     --save_outputs False \
-    --save_model False \
+    --save_model True \
     --save_hist True \
+    &
 
 
