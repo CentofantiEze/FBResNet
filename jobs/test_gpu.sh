74a801f4fb146d05def71545c11ba1d6bae3edf0
Ezequiel Centofanti <ezecentofanti@gmail.com>
2022-12-08 15:15:22 +0100

    Increase batchsize and train the model.

--- jobs/test_gpu.sh
+++ jobs/test_gpu.sh
@@ -10,16 +10,16 @@ python ../scripts/FBRN_click.py \
     --p 1 \
     --n 2000 \
     --m 50 \
-    --model_id test_gpu_ \
+    --model_id test_gpu_debug_ \
     --nb_blocks 20 \
     --im_set Set1 \
     --noise 0.05 \
     --constraint cube \
-    --train_size 400 \
-    --val_size 200 \
-    --batch_size 1 \
-    --lr 1e-3 \
-    --nb_epochs 30 \
+    --train_size 256 \
+    --val_size 64 \
+    --batch_size 32 \
+    --lr 1e-2 \
+    --nb_epochs 10 \
     --freq_val 1 \
     --loss_elt False \
     --save_signals False \
