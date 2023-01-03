7c78cd719c3f0ad933f349a993faa8ac25410eb9
Ezequiel Centofanti <ezecentofanti@gmail.com>
2023-01-03 09:20:25 +0100

    Script for training the model with the best hyperparams.

---
+++ jobs/optimal_hyperparam.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+python ../scripts/FBRN_click.py \
+    --optuna False \
+    --train_opt True \
+    --test_opt True \
+    --plot_opt True \
+    --dataset_folder ../Datasets/ \
+    --output_folder /workspace/repos/FBResNet/test_dir/ \
+    --a 1 \
+    --p 1 \
+    --n 2000 \
+    --m 50 \
+    --model_id optimal_params_ \
+    --nb_blocks 15 \
+    --im_set Set1 \
+    --noise 0.05 \
+    --constraint cube \
+    --train_size 400 \
+    --val_size 200 \
+    --batch_size 64 \
+    --lr 3e-1 \
+    --nb_epochs 90 \
+    --freq_val 1 \
+    --loss_elt False \
+    --save_signals False \
+    --save_outputs True \
+    --save_model True \
+    --save_hist True \
