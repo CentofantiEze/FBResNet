6b7a77e173213b8e565ae3b470e876199864bfcd
Ezequiel Centofanti <ezecentofanti@gmail.com>
2023-02-06 12:10:05 +0100

    Local testing jobs.

---
+++ jobs/test_gpu_local.sh
@@ -0,0 +1,28 @@
+#! /bin/sh
+
+python ../scripts/FBRN_click.py \
+    --train_opt True \
+    --test_opt True \
+    --plot_opt True \
+    --dataset_folder ../Datasets/ \
+    --output_folder C:/Users/ezece/OneDrive/Documents/Inria-OPIS/repos/FBResNet/test_dir/ \
+    --a 1 \
+    --p 1 \
+    --n 2000 \
+    --m 50 \
+    --model_id test_gpu_local_ \
+    --nb_blocks 20 \
+    --im_set Set1 \
+    --noise 0.05 \
+    --constraint cube \
+    --train_size 400 \
+    --val_size 200 \
+    --batch_size 64 \
+    --lr 1e-1 \
+    --nb_epochs 60 \
+    --freq_val 1 \
+    --loss_elt False \
+    --save_signals False \
+    --save_outputs True \
+    --save_model True \
+    --save_hist True \
